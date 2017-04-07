9ba95f020f0ac6b973fe428084ad0ca811cf1372
Johan MARTINSSON <martinsson.johan@gmail.com>
2017-04-07 09:15:06 +0200

    make it a node module extract class game

--- javascript/game.spec.js
+++ javascript/game.spec.js
@@ -1,7 +1,34 @@
-require('./game.js');
+const Game = require('./game.js').Game;
+const assert = require('assert')
+const _ = require('lodash')
 
-describe("The test environment", function() {
-  it("should pass", function() {
+describe("AskQuestion", function() {
+    const expectedPlaceCategoryMapping = {
+        0 : "Pop",
+        1 : "Science",
+        2 : "Sports",
+        3 : "Rock",
+        4 : "Culture",
+        5 : "History",
+        6 : "Pop",
+        7 : "Science",
+        8 : "Sports",
+        9 : "Rock",
+        10: "Culture",
+        11: "History",
+        12: "Pop",
+        13: "Science",
+        14: "Sports",
+        15: "Rock",
+        16: "Culture",
+        17: "History",
+    };
+    _.forEach(expectedPlaceCategoryMapping, function (category, place) {
+       it("provides the correct category", () => {
+           assert.equal(Game.askQuestion(), category)
+       })
+    })
+  it("provides the correct category", function() {
     expect(true).toBe(true);
   });
 
---
+++ javascript/game_runner.js
@@ -0,0 +1,25 @@
+"use strict"
+
+module.exports = typeof window !== "undefined" && window !== null ? window : global;
+let Game = require('./game').Game
+
+
+var notAWinner = false;
+
+var game = new Game();
+
+game.add('Chet');
+game.add('Pat');
+game.add('Sue');
+
+do{
+
+  game.roll(Math.floor(Math.random()*6) + 1);
+
+  if(Math.floor(Math.random()*10) == 7){
+    notAWinner = game.wrongAnswer();
+  }else{
+    notAWinner = game.wasCorrectlyAnswered();
+  }
+
+}while(notAWinner);
